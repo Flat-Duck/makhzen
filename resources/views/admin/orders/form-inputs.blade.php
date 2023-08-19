8c5c696b33cfb67eb00fa09ca5ccd5f606b9a5d5
Abdulrahman Mahidwei <a.mahidwei@outlook.com>
2023-08-19 13:47:04 +0200

    fix

--- resources/views/admin/orders/form-inputs.blade.php
+++ resources/views/admin/orders/form-inputs.blade.php
@@ -1,18 +1,18 @@
 @php $editing = isset($order) @endphp
 
 <x-inputs.group class="col-sm-12">
-    <x-inputs.number
+    <x-inputs.text
         name="number"
-        label={{trans('crud.orders.inputs.number')}}
+        label="{{trans('crud.orders.inputs.number')}}"
         :value="old('number', ($editing ? $order->number : ''))"
         max="255"
-        placeholder={{trans('crud.orders.inputs.number')}}
+        placeholder="{{trans('crud.orders.inputs.number')}}"
         required
-    ></x-inputs.number>
+    ></x-inputs.text>
 </x-inputs.group>
 
 <x-inputs.group class="col-sm-12">
-    <x-inputs.select name="office_id" label={{trans('crud.orders.inputs.office_id')}} required>
+    <x-inputs.select name="office_id" label="{{trans('crud.orders.inputs.office_id')}}" required>
         @php $selected = old('office_id', ($editing ? $order->office_id : '')) @endphp
         <option disabled {{ empty($selected) ? 'selected' : '' }}>الرجاء اختيار المكتب</option>
         @foreach($offices as $value => $label)
@@ -22,7 +22,7 @@
 </x-inputs.group>
 
 <x-inputs.group class="col-sm-12">
-    <x-inputs.select name="user_id" label={{trans('crud.orders.inputs.user_id')}} required>
+    <x-inputs.select name="user_id" label="{{trans('crud.orders.inputs.user_id')}}" required>
         @php $selected = old('user_id', ($editing ? $order->user_id : '')) @endphp
         <option disabled {{ empty($selected) ? 'selected' : '' }}>  الرجاء اختيار المستخدم</option>
         @foreach($users as $value => $label)
